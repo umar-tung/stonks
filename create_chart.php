d570655c01fc20a6f316b4f09ac0dea6920e5c11
umar <umartung@gmail.com>
2020-09-05 22:33:20 -0700

    add graph

---
+++ create_chart.php
@@ -0,0 +1,9 @@
+<?php 
+public function indexAction($stock_data){
+  $time_series = &$stock_data["Monthly Time Series"];
+
+  foreach($time_series as $key => $value){
+    
+  }
+}
+?>
--- index.php
+++ index.php
@@ -10,6 +10,8 @@
 
     <title>Stonks!</title>
   </head>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.3.0/Chart.bundle.js"></script>
   <?php
     require_once __DIR__.'/vendor/autoload.php';
     require __DIR__.'/api_call.php'; 
@@ -23,10 +25,30 @@
     $response="";
     $api_request_url = "";
     $another_response = "";
+    $dates=array();
+    $open=array();
+    $high=array();
+    $low=array();
+    $close=array();
     if($_SERVER["REQUEST_METHOD"] == "POST"){
       $stock = $_POST["stock"];
       $api_request_url = $api_url."?"."function=".$function."&symbol=".$stock."&apikey=".$api_key;
       $response = file_get_contents($api_request_url);
+      $response_arr = json_decode($response, true);
+      $monthly_data = $response_arr["Monthly Time Series"];
+      $i=0; 
+      foreach($monthly_data as $date => $value){
+        $dates[]=$date;
+        $open[]= $value["1. open"];
+        $high[]= $value["2. high"];
+        $low[]= $value["3. low"];
+        $close[]= $value["4. close"];
+        $i++;
+        if($i===12){
+          break;
+        }        
+      }     
+       
        
     }
   ?>
@@ -39,9 +61,47 @@
     </form>
     <p>The api url: <?=$api_url?></p>
     <p>The api key: <?=$api_key?></p>
-    <p>The stock data: <?=$stock_data?></p>
     <p>The request url: <?=$api_request_url?></p>
-
+    <div class="container">
+      <canvas id="myChart" width="100" height="100"></canvas>
+    </div> 
+    <script> 
+      var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: [<?php foreach(array_reverse($dates) as $date){echo "\"".$date."\"".',';}?>],
+          datasets: [
+            {
+              label: 'open',
+              data: [<?php foreach(array_reverse($open) as $value){echo $value.',';}?>],
+              fill: false,
+              borderColor: "rgb(75,192,192)"
+            },
+            {
+              label: 'close',
+              data: [<?php foreach(array_reverse($close) as $value){echo $value.',';}?>],
+              fill: false,
+              borderColor: "rgb(75,100,100)"
+            },
+            {
+              label: 'high',
+              data: [<?php foreach(array_reverse($high) as $value){echo $value.',';}?>],
+              fill: false,
+              borderColor: "rgb(205,226,192)"
+            },
+            {
+              label: 'low',
+              data: [<?php foreach(array_reverse($low) as $value){echo $value.',';}?>],
+              fill: false,
+              borderColor: "rgb(0,0,255)"
+            },
+              
+          ],
+        }
+      });
+    </script>
+    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
